b7d711653ee38d69dd213cb100d9a602fccd4002
Chris Chalstrom <cchalstr@nsidc-chalstrom.ad.int.nsidc.org>
2015-02-10 15:18:42 -0700

    update solr url to search-solr :8983, add simple-themes-plugin to ci.yaml

--- config/app_config.rb
+++ config/app_config.rb
@@ -36,31 +36,31 @@ module AppConfig
       query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_integration.yml'))
     },
     integration: COMMON.clone.merge(
-                                      solr_url: 'http://integration.solr-search.apps.int.nsidc.org:9283/solr/nsidc_oai',
-                                      solr_auto_suggest_url: 'http://integration.solr-search.apps.int.nsidc.org:9283/solr/auto_suggest',
-                                      dataset_catalog_services_url: 'http://integration.nsidc.org/api/dataset/metadata',
-                                      metrics_url: 'http://liquid.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/integration',
-                                      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_integration.yml'))
-                                    ),
+      solr_url: 'http://integration.search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai',
+      solr_auto_suggest_url: 'http://integration.search-solr.apps.int.nsidc.org:8983/solr/auto_suggest',
+      dataset_catalog_services_url: 'http://integration.nsidc.org/api/dataset/metadata',
+      metrics_url: 'http://liquid.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/integration',
+      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_integration.yml'))
+    ),
     qa: COMMON.clone.merge(
-                             solr_url: 'http://qa.solr-search.apps.int.nsidc.org:9283/solr/nsidc_oai',
-                             solr_auto_suggest_url: 'http://qa.solr-search.apps.int.nsidc.org:9283/solr/auto_suggest',
-                             dataset_catalog_services_url: 'http://qa.nsidc.org/api/dataset/metadata',
-                             metrics_url: 'http://brash.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/qa',
-                             query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_qa.yml'))
-                           ),
+      solr_url: 'http://qa.search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai',
+      solr_auto_suggest_url: 'http://qa.search-solr.apps.int.nsidc.org:8983/solr/auto_suggest',
+      dataset_catalog_services_url: 'http://qa.nsidc.org/api/dataset/metadata',
+      metrics_url: 'http://brash.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/qa',
+      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_qa.yml'))
+    ),
     staging: COMMON.clone.merge(
-                                  solr_url: 'http://staging.solr-search.apps.int.nsidc.org:9283/solr/nsidc_oai',
-                                  solr_auto_suggest_url: 'http://staging.solr-search.apps.int.nsidc.org:9283/solr/auto_suggest',
-                                  dataset_catalog_services_url: 'http://staging.nsidc.org/api/dataset/metadata',
-                                  query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_production.yml'))
-                                ),
+      solr_url: 'http://staging.search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai',
+      solr_auto_suggest_url: 'http://staging.search-solr.apps.int.nsidc.org:8983/solr/auto_suggest',
+      dataset_catalog_services_url: 'http://staging.nsidc.org/api/dataset/metadata',
+      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_production.yml'))
+    ),
     production: COMMON.clone.merge(
-                                     solr_url: 'http://solr-search.apps.int.nsidc.org:9283/solr/nsidc_oai',
-                                     solr_auto_suggest_url: 'http://solr-search.apps.int.nsidc.org:9283/solr/auto_suggest',
-                                     dataset_catalog_services_url: 'http://nsidc.org/api/dataset/metadata',
-                                     metrics_url: 'http://frozen.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/prod',
-                                     query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_production.yml'))
-                                   )
+      solr_url: 'http://search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai',
+      solr_auto_suggest_url: 'http://search-solr.apps.int.nsidc.org:8983/solr/auto_suggest',
+      dataset_catalog_services_url: 'http://nsidc.org/api/dataset/metadata',
+      metrics_url: 'http://frozen.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/prod',
+      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_production.yml'))
+    )
   }
 end
