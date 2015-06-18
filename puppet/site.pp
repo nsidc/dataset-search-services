a39411dcd3842f3a02d2904f34b9ac09fcd5b04f
Michael Brandt <mbrandt@colorado.edu>
2015-06-18 12:51:25 -0600

    Upgrade to Ruby 2.2.2, update Gems
    Puppetry to install Ruby 2.2
    
    - use only the same nokogiri deps as search_solr_tools
    
    - don't install nsidc_os_repo (see output below for provisioning
      failure)
    
    Update gems
    
    - remove version restrictions on rspec and turnip_formatter
    
    - remove rspec-spies
    
    - add pry-byebug
    
    - remove Gemfile.lock to upgrade all unpegged gems (and add the new
      lockfile)
    
    ========================================================================
    
    This is all of the puppet provisioning output that was red:
    
    ```
    ==> default: Error: Execution of '/usr/bin/apt-get -q -y -o DPkg::Options::=--force-confold install ruby2.2' returned 100: Reading package lists...
    ==> default: Building dependency tree...
    ==> default: Reading state information...
    ==> default: Some packages could not be installed. This may mean that you have
    ==> default: requested an impossible situation or if you are using the unstable
    ==> default: distribution that some required packages have not yet been created
    ==> default: or been moved out of Incoming.
    ==> default: The following information may help to resolve the situation:
    ==> default:
    ==> default: The following packages have unmet dependencies:
    ==> default:  ruby2.2 : Depends: rubygems-integration (>= 1.8) but 1.5 is to be installed
    ==> default:            Recommends: libjs-jquery but it is not going to be installed
    ==> default: E: Unable to correct problems, you have held broken packages.
    ==> default: Error: /Stage[main]/Main/Package[ruby2.2]/ensure: change from purged to present failed: Execution of '/usr/bin/apt-get -q -y -o DPkg::Options::=--force-confold install ruby2.2' returned 100: Reading package lists...
    ==> default: Building dependency tree...
    ==> default: Reading state information...
    ==> default: Some packages could not be installed. This may mean that you have
    ==> default: requested an impossible situation or if you are using the unstable
    ==> default: distribution that some required packages have not yet been created
    ==> default: or been moved out of Incoming.
    ==> default: The following information may help to resolve the situation:
    ==> default:
    ==> default: The following packages have unmet dependencies:
    ==> default:  ruby2.2 : Depends: rubygems-integration (>= 1.8) but 1.5 is to be installed
    ==> default:            Recommends: libjs-jquery but it is not going to be installed
    ==> default: E: Unable to correct problems, you have held broken packages.
    ==> default: Warning: /Package[ruby2.2-dev]: Skipping because of failed dependencies
    ==> default: Warning: /Stage[main]/Main/Exec[install bundler]: Skipping because of failed dependencies
    ==> default: Warning: /Package[zlib1g-dev]: Skipping because of failed dependencies
    ```

--- puppet/site.pp
+++ puppet/site.pp
@@ -4,40 +4,28 @@ hiera_include('classes')
 # Ensure the brightbox apt repository gets added before installing ruby
 include apt
 apt::ppa{'ppa:brightbox/ruby-ng':}
-class {'ruby':
-  require         => [ Class['apt'], Apt::Ppa['ppa:brightbox/ruby-ng'] ],
-  version => '1.9.3',
-  set_system_default => true
-}
-class {'ruby::dev':
-  require         => [ Class['apt'], Apt::Ppa['ppa:brightbox/ruby-ng'] ]
-}
 
-### BEGIN nokogiri deps
-class update-package-manager {
-  exec { "update":
-    path => "/bin:/usr/bin:/usr/local/bin:/usr/local/sbin:usr/sbin:/sbin:/usr/java/jdk/bin",
-    command => "apt-get -y update; sudo apt-get -y install libxml2 libxml2-dev libxslt1-dev"
-  }
-  notify { "apt-get update complete":
-    require => Exec['update']
-  }
-}
-
-Class['update-package-manager'] -> Package <| |>
-
-package {"libssl-dev":
+package { 'ruby2.2':
+  ensure => present,
+  require => [ Class['apt'], Apt::Ppa['ppa:brightbox/ruby-ng'] ]
+} ->
+package { 'ruby2.2-dev':
   ensure => present
 } ->
-package {"build-essential":
+exec { 'install bundler':
+  command => 'sudo gem install bundler -v 1.10.3',
+  path => '/usr/bin'
+} ->
+
+# puma native extension dep
+package { 'libssl-dev':
   ensure => present
 } ->
-package {"libxml2-dev":
+
+# nokogiri native extension dep
+package { 'zlib1g-dev':
   ensure => present
 }
-
-include update-package-manager
-### END nokogiri deps
 
 # vagrant must be able to write to /var/log
 user { 'vagrant':
