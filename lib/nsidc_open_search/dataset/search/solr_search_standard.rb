0b553e4681bed6d1eafc4254cbbaead26e7fadbd
Michael Brandt <mbrandt@colorado.edu>
2015-06-19 16:38:02 -0600

    Refactor for RuboCop: Metrics/ClassLength

--- lib/nsidc_open_search/dataset/search/solr_search_standard.rb
+++ lib/nsidc_open_search/dataset/search/solr_search_standard.rb
@@ -26,21 +26,19 @@ module NsidcOpenSearch
         OPERATOR = 'AND'
 
         def build_fields(fields, boosts)
-          boosted_fields = fields.map do |f|
-            if boosts.key?(f.to_s)
-              "#{f}^#{boosts[f.to_s]}"
+          fields.map do |field|
+            if boosts.key?(field.to_s)
+              "#{field}^#{boosts[field.to_s]}"
             else
-              f
+              field
             end
-          end
-          boosted_fields.join(' ')
+          end.join(' ')
         end
 
         def build_fq_parameter(search_params)
           filters = ["source:#{search_params[:source]}"]
           begin
-            parsed_filter = JSON.parse(search_params[:facetFilters])
-            parsed_filter.each do |k, v|
+            JSON.parse(search_params[:facetFilters]).each do |k, v|
               filters << "{!tag=#{k}}#{k}:#{get_facet_filter(v)}"
             end
           rescue
@@ -142,8 +140,7 @@ module NsidcOpenSearch
           else
             # Otherwise, build an operator separated list of the formatted terms
             operator_joined_terms(
-              build_generic_terms(generic_terms) <<
-                build_spatial_term(field_terms) <<
+              build_generic_terms(generic_terms) << build_spatial_term(field_terms) <<
                 build_temporal_term(field_terms)
             )
           end
@@ -157,57 +154,57 @@ module NsidcOpenSearch
         end
 
         def build_spatial_term(field_terms)
-          if field_terms.key? :spatial
-            # Expect a spatial search string with the format
-            # westLon,southLat,eastLon,northLat this allows us to easily
-            # construct a Lucene Spatial query with the format [southLat,westLon
-            # TO northLat,eastLon]
-            coords = field_terms[:spatial].split(',')
-            fail 'Invalid spatial search. Input must have the format westLon,southLat,eastLon,'\
-                 'northLat' unless coords.length == 4
-            "spatial:[#{coords[1]},#{coords[0]} TO #{coords[3]},#{coords[2]}]"
-          end
+          return unless field_terms.key?(:spatial)
+
+          # Expect a spatial search string with the format
+          # westLon,southLat,eastLon,northLat this allows us to easily
+          # construct a Lucene Spatial query with the format [southLat,westLon
+          # TO northLat,eastLon]
+          coords = field_terms[:spatial].split(',')
+          fail 'Invalid spatial search. Input must have the format westLon,southLat,eastLon,'\
+               'northLat' unless coords.length == 4
+          "spatial:[#{coords[1]},#{coords[0]} TO #{coords[3]},#{coords[2]}]"
         end
 
+        # Solr indexes temporal ranges as points in a spatial field.
+        # - the start and end date will each have the format YY.YYMMDD
+        # - the start date needs to be converted to a latitude
+        # - the end date needs to be converted to a longitude
+        #
+        # - To find all datasets with ranges that intersect the start date and
+        #   end date range, create a query with the format [start_date,0 TO
+        #   90,end_date], which means search every thing with start and end
+        #   dates within the given range.
+        #
+        # - To find all datasets with ranges that intersect the start date,
+        #   create a query with the format [start_date,0 TO 90,180], which means
+        #   search every thing with an end date after the given start date.
+        #
+        # - To find all datasets with ranges that intersect the end date, create
+        #   a query with the format [0,0 TO 90,enddate], which means search every
+        #   thing with a start date before the given end date.
         def build_temporal_term(field_terms)
-          # Solr indexes temporal ranges as points in a spatial field.
-          # The start date needs to be converted to a latitude and will have the
-          #   format YY.YYMMDD
-          # The end date needs to be converted to a longitude and will have the
-          #   format YY.YYMMDD
-          if field_terms.key?(:startDate) && field_terms.key?(:endDate)
-            # To find all datasets with ranges that intersect the start date and
-            # end date range create a query with the format [start_date,0 TO
-            # 90,end_date, which means search every thing with an end date after
-            # the start date.
-            'temporal:['\
-              "#{format_temporal_start(field_terms[:startDate])},0 TO "\
-              "90,#{format_temporal_end(field_terms[:endDate])}"\
-              ']'
-          elsif field_terms.key? :startDate
-            # Solr indexes temporal ranges as points in a spatial field. To find
-            # all datasets with ranges that intersect the start date create a
-            # query with the format [start_date,0 TO 90,180], which means search
-            # every thing with an end date after the start date.
-            "temporal:[#{format_temporal_start(field_terms[:startDate])},0 TO 90,180]"
-          elsif field_terms.key? :endDate
-            # Solr indexes temporal ranges as points in a spatial field. To find
-            # all datasets with ranges that intersect the end date, create a
-            # query with the format [0,0 TO 90,enddate], which means search
-            # every thing with a start date before the end dates.
-            "temporal:[0,0 TO 90,#{format_temporal_end(field_terms[:endDate])}]"
+          return if [:startDate, :endDate].none?(&field_terms.method(:key?))
+
+          if field_terms.key?(:startDate)
+            formatted_start = format_temporal_start(field_terms[:startDate])
           end
+
+          if field_terms.key?(:endDate)
+            formatted_end = format_temporal_end(field_terms[:endDate])
+          end
+
+          "temporal:[#{formatted_start || '0'},0 TO 90,#{formatted_end || '180'}]"
         end
 
         def build_terms(terms)
-          t = terms.map do |k, v|
+          terms.map do |k, v|
             if k.eql? :searchTerms
               format_query_term(v)
             else
               "#{k}:#{format_query_term(v)}"
             end
-          end
-          t.reject(&:nil?).join(" #{OPERATOR} ")
+          end.reject(&:nil?).join(" #{OPERATOR} ")
         end
 
         def format_query_term(term, include_params = true)
@@ -247,11 +244,11 @@ module NsidcOpenSearch
           # "va2")'
           # ref: https://github.com/mwmitchell/rsolr-ext/blob/master/lib/rsolr-ext/request.rb
           # ref: http://stackoverflow.com/questions/5296441/passing-comma-seperated-values-in-filter-query-of-solr-response
-          "(\"#{values.join('" "')}\")"
+          %[("#{values.join('" "')}")]
         end
 
         def get_terms(search_params, key_list)
-          search_params.dup.keep_if { |k, v| key_list.include?(k) && !v.nil_or_whitespace? }
+          search_params.select { |k, v| key_list.include?(k) && !v.nil_or_whitespace? }
         end
 
         def operator_joined_terms(terms_arr)
