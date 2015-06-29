nsidc/dataset-search-services

commit 16ce32b99e593d2c8181d0b028fd06f2c3e6e588

Refactor for RuboCop: Metrics/LineLength

config/app_config.rb
@@ -4,63 +4,20 @@ require('yaml')
 module AppConfig
   APPLICATION_NAME = 'dataset-search-services'
   APP_PATH = '/opt/search_services'
+  APP_CONFIGS = YAML.load_file(File.expand_path('../app_config.yaml', __FILE__))
 
-  def self.[](key)
-    key_sym = key.to_sym unless key.nil?
-    key_sym = :development if APPLICATION_ENVIRONMENTS[key_sym].nil?
+  def self.[](env = :development)
+    if env.to_sym == :development
+      app_config = APP_CONFIGS[:development]
+      query_config_env = :integration
+    else
+      app_config = APP_CONFIGS[:common].merge(APP_CONFIGS[env.to_sym])
+      query_config_env = env
+    end
 
-    APPLICATION_ENVIRONMENTS[key_sym]
+    query_config_file = File.expand_path("../solr_query_config_#{query_config_env}.yml", __FILE__)
+    app_config[:query_config] = YAML.load_file(query_config_file)
+
+    app_config
   end
-
-  private
-
-  COMMON = {
-    enricher_thread_count: 5,
-    relative_url_root: '/api/dataset/2',
-    port: '10680',
-    num_workers: 5,
-    pidfile: '/var/run/puma/search_services.pid',
-    state_path: '/var/run/puma/search_services.state',
-    std_err_log: '/var/log/search_services.stderr.log',
-    std_out_log: '/var/log/search_services.stdout.log'
-  }
-
-  APPLICATION_ENVIRONMENTS = {
-    development: {
-      enricher_thread_count: 5,
-      relative_url_root: '/',
-      solr_url: 'http://localhost:8983/solr/nsidc_oai',
-      solr_auto_suggest_url: 'http://localhost:8983/solr/auto_suggest',
-      dataset_catalog_services_url: 'http://integration.nsidc.org/api/dataset/metadata',
-      metrics_url: 'http://liquid.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/integration',
-      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_integration.yml'))
-    },
-    integration: COMMON.clone.merge(
-      solr_url: 'http://integration.search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai',
-      solr_auto_suggest_url: 'http://integration.search-solr.apps.int.nsidc.org:8983/solr/auto_suggest',
-      dataset_catalog_services_url: 'http://integration.nsidc.org/api/dataset/metadata',
-      metrics_url: 'http://liquid.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/integration',
-      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_integration.yml'))
-    ),
-    qa: COMMON.clone.merge(
-      solr_url: 'http://qa.search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai',
-      solr_auto_suggest_url: 'http://qa.search-solr.apps.int.nsidc.org:8983/solr/auto_suggest',
-      dataset_catalog_services_url: 'http://qa.nsidc.org/api/dataset/metadata',
-      metrics_url: 'http://brash.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/qa',
-      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_qa.yml'))
-    ),
-    staging: COMMON.clone.merge(
-      solr_url: 'http://staging.search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai',
-      solr_auto_suggest_url: 'http://staging.search-solr.apps.int.nsidc.org:8983/solr/auto_suggest',
-      dataset_catalog_services_url: 'http://staging.nsidc.org/api/dataset/metadata',
-      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_production.yml'))
-    ),
-    production: COMMON.clone.merge(
-      solr_url: 'http://search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai',
-      solr_auto_suggest_url: 'http://search-solr.apps.int.nsidc.org:8983/solr/auto_suggest',
-      dataset_catalog_services_url: 'http://nsidc.org/api/dataset/metadata',
-      metrics_url: 'http://frozen.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/prod',
-      query_config: YAML.load_file(File.join(File.dirname(__FILE__), 'solr_query_config_production.yml'))
-    )
-  }
 end

.rubocop.yml
@@ -16,7 +16,7 @@ Metrics/AbcSize:
   - lib/nsidc_open_search/dataset_osdd.rb
 
 Metrics/LineLength:
-  Enabled: false
+  Max: 100
 
 Metrics/MethodLength:
   Max: 15

.rubocop_todo.yml
@@ -1,5 +1,5 @@
 # This configuration was generated by `rubocop --auto-gen-config`
-# on 2015-06-22 15:28:45 -0600 using RuboCop version 0.32.0.
+# on 2015-06-22 15:56:20 -0600 using RuboCop version 0.32.0.
 # The point is for the user to remove these configuration records
 # one by one as the offenses are removed from the code base.
 # Note that changes in the inspected code, or installation of new
@@ -12,13 +12,13 @@ Lint/UselessAssignment:
 # Offense count: 1
 # Configuration parameters: CountComments.
 Metrics/ClassLength:
-  Max: 113
+  Max: 132
 
 # Offense count: 1
 Style/AccessorMethodName:
   Enabled: false
 
-# Offense count: 14
+# Offense count: 12
 # Cop supports --auto-correct.
 # Configuration parameters: EnforcedStyle, SupportedStyles, ProceduralMethods, FunctionalMethods, IgnoredMethods.
 Style/BlockDelimiters:
@@ -29,12 +29,7 @@ Style/BlockDelimiters:
 Style/ClassAndModuleChildren:
   Enabled: false
 
-# Offense count: 2
-# Cop supports --auto-correct.
-Style/EmptyLiteral:
-  Enabled: false
-
-# Offense count: 3
+# Offense count: 4
 # Configuration parameters: MinBodyLength.
 Style/GuardClause:
   Enabled: false

Guardfile
@@ -9,7 +9,13 @@ guard :rubocop do
   watch(/(?:.+\/)?\.rubocop\.yml/) { |m| File.dirname(m[0]) }
 end
 
-guard 'puma', bind: 'tcp://0.0.0.0:3000', threads: '1:1', workers: 5, environment: 'development', config: '-' do
+guard('puma',
+      bind: 'tcp://0.0.0.0:3000',
+      threads: '1:1',
+      workers: 5,
+      environment: 'development',
+      config: '-'
+     ) do
   watch('Gemfile.lock')
   watch(/^config|lib\/.*/)
 end

Rakefile
@@ -19,8 +19,10 @@ task :run do
   # require gems here to avoid conflicts between rake and sinatra-contrib
   Bundler.require(:default)
 
-  # The following line should enable an ssl connection, but it currently doesn't work https://github.com/puma/puma/issues/522
-  # sh 'puma -b ssl://0.0.0.0:3000\?key=/home/vagrant/tmp/cert/server.key\&cert=/home/vagrant/tmp/cert/server.csr -t 1:1 -w 5 -e development -C "-"'
+  # The following lines should enable an ssl connection, but it currently
+  # doesn't work https://github.com/puma/puma/issues/522
+  # sh 'puma -b ssl://0.0.0.0:3000\?key=/home/vagrant/tmp/cert/server.key\&cert='\
+  #    '/home/vagrant/tmp/cert/server.csr -t 1:1 -w 5 -e development -C "-"'
 
   sh 'puma -b tcp://0.0.0.0:3000 -t 1:1 -w 5 -e development -C "-"'
 end

config/app_config.yaml
@@ -1,0 +1,40 @@
+:common:
+  :enricher_thread_count: 5
+  :relative_url_root: /api/dataset/2
+  :port: '10680'
+  :num_workers: 5
+  :pidfile: /var/run/puma/search_services.pid
+  :state_path: /var/run/puma/search_services.state
+  :std_err_log: /var/log/search_services.stderr.log
+  :std_out_log: /var/log/search_services.stdout.log
+
+:development:
+  :enricher_thread_count: 5
+  :relative_url_root: /
+  :solr_url: http://localhost:8983/solr/nsidc_oai
+  :solr_auto_suggest_url: http://localhost:8983/solr/auto_suggest
+  :dataset_catalog_services_url: http://integration.nsidc.org/api/dataset/metadata
+  :metrics_url: http://liquid.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/integration
+
+:integration:
+  :solr_url: http://integration.search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai
+  :solr_auto_suggest_url: http://integration.search-solr.apps.int.nsidc.org:8983/solr/auto_suggest
+  :dataset_catalog_services_url: http://integration.nsidc.org/api/dataset/metadata
+  :metrics_url: http://liquid.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/integration
+
+:qa:
+  :solr_url: http://qa.search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai
+  :solr_auto_suggest_url: http://qa.search-solr.apps.int.nsidc.org:8983/solr/auto_suggest
+  :dataset_catalog_services_url: http://qa.nsidc.org/api/dataset/metadata
+  :metrics_url: http://brash.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/qa
+
+:staging:
+  :solr_url: http://staging.search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai
+  :solr_auto_suggest_url: http://staging.search-solr.apps.int.nsidc.org:8983/solr/auto_suggest
+  :dataset_catalog_services_url: http://staging.nsidc.org/api/dataset/metadata
+
+:production:
+  :solr_url: http://search-solr.apps.int.nsidc.org:8983/solr/nsidc_oai
+  :solr_auto_suggest_url: http://search-solr.apps.int.nsidc.org:8983/solr/auto_suggest
+  :dataset_catalog_services_url: http://nsidc.org/api/dataset/metadata
+  :metrics_url: http://frozen.colorado.edu:12180/metrics/projects/search/services/dataset-search-services/instances/prod

config/deployment_config.rb
@@ -29,7 +29,8 @@ module DeploymentConfig
     app_name: AppConfig::APPLICATION_NAME,
     deployment_directory: AppConfig::APPLICATION_NAME,
     artifact_repo: "/disks/integration/san/INTRANET/REPO/#{AppConfig::APPLICATION_NAME}",
-    deployment_log: "/disks/integration/san/INTRANET/REPO/#{AppConfig::APPLICATION_NAME}/deployable_versions_vm",
+    deployment_log: "/disks/integration/san/INTRANET/REPO/#{AppConfig::APPLICATION_NAME}/"\
+                    'deployable_versions_vm',
     deploy_dirs: %w(config lib config.ru Gemfile Gemfile.lock README.md deployment)
   }
 

config/puma.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env puma
-require File.join(File.dirname(__FILE__), 'app_config')
+require_relative 'app_config'
 
-# Puma configuration file. See https://github.com/puma/puma for more information and example config files.
+# Puma configuration file. See https://github.com/puma/puma for more information
+# and example config files.
 
 env = "#{ENV['RACK_ENV']}" || 'development'
 
@@ -13,4 +14,8 @@ threads 1, 1 # min, max
 workers AppConfig[env][:num_workers]
 environment env
 # STDOUT, STDERR, append
-stdout_redirect '/live/apps/dataset-search-services/run/logs/puma.stdout.log', '/live/apps/dataset-search-services/run/logs/puma.stderr.log', true
+stdout_redirect(
+  '/live/apps/dataset-search-services/run/logs/puma.stdout.log',
+  '/live/apps/dataset-search-services/run/logs/puma.stderr.log',
+  true
+)

commit 87e34cbf066289d5e2d35494cd7523857a8067f2

Fix config for staging environment.
This was broken in 16ce32b when I failed to note that the staging
configuration actually used `solr_query_config_production.yml`; after
that change, staging was configured to use
`solr_query_config_staging.yml`, which sadly does not exist.

config/app_config.rb
@@ -12,7 +12,7 @@ module AppConfig
       query_config_env = :integration
     else
       app_config = APP_CONFIGS[:common].merge(APP_CONFIGS[env.to_sym])
-      query_config_env = env
+      query_config_env = env.to_sym == :staging ? :production : env
     end
 
     query_config_file = File.expand_path("../solr_query_config_#{query_config_env}.yml", __FILE__)